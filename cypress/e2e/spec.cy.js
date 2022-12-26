fe47c8e562e26f7c00fac488655ba9f04b32fe9c
damonholden <damonh003@gmail.com>
2022-12-26 21:16:47 +0000

    test: add e2e test for lifecycle callback demo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -52,4 +52,60 @@ describe('page', () => {
         });
     });
   });
+
+  it('should correctly display custom the lifecycle callback demo', () => {
+    cy.visit('/');
+    cy.get('#lifecycle-callback-demo-section').within(() => {
+      const boxProps = {
+        width: null,
+        height: null,
+        color: null,
+      };
+      cy.get('h2').should('contain', 'Lifecycle Callbacks Demo');
+      cy.get('div.container').as('container').should('exist').and('be.empty');
+      cy.get('span.message').as('message').should('exist').should('be.empty');
+      cy.get('button.add').click();
+      cy.get('@container')
+        .should('not.be.empty')
+        .within(() => {
+          cy.get('custom-square')
+            .should('exist')
+            .and('be.visible')
+            .then(($square) => {
+              boxProps.width = $square[0].getAttribute('l');
+              boxProps.height = $square[0].getAttribute('l');
+              boxProps.color = $square[0].getAttribute('c');
+            });
+        });
+      cy.get('@message')
+        .should('not.be.empty')
+        .and('have.text', 'custom square element added to page');
+      cy.get('button.update').click();
+      cy.get('@container')
+        .should('not.be.empty')
+        .within(() => {
+          cy.get('custom-square')
+            .should('exist')
+            .and('be.visible')
+            .then(($square) => {
+              console.log($square);
+              expect($square[0].getAttribute('l')).not.to.equal(boxProps.width);
+              expect($square[0].getAttribute('l')).not.to.equal(
+                boxProps.height,
+              );
+              expect($square[0].getAttribute('c')).not.to.equal(boxProps.color);
+            });
+        });
+      cy.get('@message').should(
+        'have.text',
+        'custom square element attributes changed',
+      );
+      cy.get('button.remove').click();
+      cy.get('@container').should('be.empty');
+      cy.get('@message').should(
+        'have.text',
+        'custom square element removed from page',
+      );
+    });
+  });
 });
